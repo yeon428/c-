f9aaf4db511af26614a1c497cbcaa1cd690fa8e3
yeon428 <jiyeonhe0401@naver.com>
2025-07-07 12:23:09 +0900

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,73 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//void main()
+//{
+//	int a, b;
+//	int result;
+//	char k;
+//
+//	printf("첫번째 계산할 값 ==>");
+//	scanf("%d", &a);
+//	printf("+ - * / % ==>");
+//	scanf(" %c", &k);
+//	printf("두번째 계산할 값 ==>");
+//	scanf("%d", &b);
+//
+//	if (k == '+') {
+//		result = a + b;
+//		printf(" %d + %d = %d\n", a, b, result);
+//	}
+//	if (k == '-') {
+//		result = a - b;
+//		printf(" %d - %d = %d\n", a, b, result);
+//	}
+//	if (k == '*') {
+//		result = a * b;
+//		printf(" %d * %d = %d\n", a, b, result);
+//	}
+//	if (k == '/') {
+//		if (b != 0) {
+//			result = a / b;
+//			printf(" %d / %d = %d\n", a, b, result);
+//		}
+//		else
+//			printf(" 0으로 나누면 안됩니다. \n");
+//	}
+//	if (k == '%') {
+//		if (b != 0) {
+//			result = a % b;
+//			printf(" %d %% %d = %d\n", a, b, result);
+//		}
+//		else
+//			printf(" 0으로 나누면 나머지값이 안됩니다. \n");
+//	}
+//
+//}
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("%d\n", 123);
+//	printf("%5d\n", 123);
+//	printf("%05d\n", 123);
+//
+//	printf("%f\n", 123.45);
+//	printf("%7.1f\n", 123.45);
+//	printf("%7.3f\n", 123.45);
+//
+//	printf("%s\n", "Basic-C");
+//	printf("%10s\n", "Basic-C");
+//}
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("\n줄 바꿈\n연습 \n");
+//	printf("\t탭키\t연습 \n");
+//	printf("이것을 \r덮어씁니다 \n");
+//	printf("\a\a\a삐소리 3번 \n");
+//	printf("글자가 \"강조\"되는 효과 \n");
+//	printf("\\\\\\ 역슬래시 세 개 출력 \n");
+//}
