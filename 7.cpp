f5e65e1063bc1f6fea39605b048e881e8a0ad0fc
yeon428 <jiyeonhe0401@naver.com>
2025-07-28 20:08:52 +0900

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int i;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("안녕하세요?\n");
+//		printf("##또 안녕하세요?##\n");
+//	}
+//
+//	printf("\n\n");
+//
+//	for (i = 0; i < 3; i++)
+//		printf("안녕하세요?\n");
+//	printf("##또 안녕하세요?## \n");
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//	int i;
+//
+//	for (i = 5; i > 0; i--)
+//	{
+//		printf("%d: 안녕하세요? 빙글빙글 for문을 공부 중입니다. ^^\n", i);
+//	}
+//}
+
+#include <stdio.h>
+void main()
+{
+	int hap;
+
+	hap = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+
+	printf("1에서 10까지의 합: %d\n", hap);
+}
