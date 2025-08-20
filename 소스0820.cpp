d2947a067949bd747971545751f758fc1271967f
yeon428 <jiyeonhe0401@naver.com>
2025-08-20 16:51:02 +0900

    Add files via upload

---
+++ 소스0820.cpp
@@ -0,0 +1,47 @@
+//#include <stdio.h>
+//int minus(int n)
+//{
+//	if (n <= 1) return -1;
+//	return minus(n - 1) - 1;
+//}
+//int main()
+//{
+//	int n; scanf("%d", &n);
+//	printf("%d", minus(n));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void printNumber(int n)
+//{
+//	if (n <= 0) return;
+//	printf("%d", n);
+//	printNumber(n - 1);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printNumber(n);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int fibonacci(int n)
+//{
+//	printf("%d", n);
+//	if (n <= 2) return 1;
+//	printf("%d\n", n);
+//	return fibonacci(n - 1) + fibonacci(n - 2);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("\n%d\n", fibonacci(n));
+//	return 0;
+//}
+
+#include <stdio.h>
+int 
